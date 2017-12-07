a6d7c07f7f10a9b2ef25ce3fb0c089c419897667
RomanBykov <30844184+RomanBykov@users.noreply.github.com>
2017-12-07 17:50:06 +0300

    Сделал по рекомендациям
    Осталось проверить работу по нажатию клавиш

--- js/map.js
+++ js/map.js
@@ -47,6 +47,8 @@ var mapFiltersArray = map.querySelectorAll('.map__filter');
 
 var mapCardClose = mapCard.querySelector('.popup__close');
 
+var activePin = false;
+
 // общий рандом
 var getRandomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max - min));
@@ -86,7 +88,8 @@ var generateCards = function () {
 
     cards[i] = {
       author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        avatar: 'img/avatars/user0' + (i + 1) + '.png',
+        number: -1 + (i + 1)
       },
       offer: {
         title: OFFERS_HEADINGS[getRandomNumber(0, 8)],
@@ -122,16 +125,10 @@ var renderMapPins = function (pin) {
   mapPin.style.left = pin.location.x + 'px';
   mapPin.style.top = pin.location.y + 'px';
   mapPin.querySelector('img').src = pin.author.avatar;
+  mapPin.setAttribute('id', pin.author.number);
 
   return mapPin;
 };
-
-// п.4 Отрисуйте сгенерированные DOM-элементы в блок .map__pins. Для вставки элементов используйте DocumentFragment
-// cards = generateCards();
-
-cards.forEach(function (item) {
-  fragmentPins.appendChild(renderMapPins(item));
-});
 
 // п.5 На основе первого по порядку элемента из сгенерированного массива и шаблона template article.map__card
 // создайте DOM-элемент объявления, заполните его данными из объекта и вставьте полученный DOM-элемент в блок
@@ -139,7 +136,8 @@ cards.forEach(function (item) {
 
 
 // заполните его данными из объекта
-var renderCards = function (card) {
+var fillCards = function (card) {
+  mapCard.querySelector('.popup__avatar').src = card.author.avatar;
   mapCard.querySelector('h3').textContent = card.offer.title;
   mapCard.querySelector('small').textContent = card.offer.address;
   mapCard.querySelector('.popup__price').innerHTML = card.offer.price + ' &#x20bd;/ночь';
@@ -149,18 +147,9 @@ var renderCards = function (card) {
   mapCard.querySelector('.popup__features').innerHTML = '';
   mapCard.querySelector('.popup__features').insertAdjacentHTML('afterbegin', card.offer.features.map(getFeatures).join(' '));
   mapCard.querySelector('.popup__description').textContent = card.offer.description;
-  mapCard.querySelector('.popup__avatar').src = card.author.avatar;
 
   return mapCard;
 };
-
-// fragmentCards.appendChild(renderCards(cards[getRandomNumber(0, 8)]));
-
-// // вставьте полученный DOM-элемент в блок .map
-// map.appendChild(fragmentCards);
-
-// В момент открытия, страница должна находиться в следующем состоянии: карта затемнена (добавлен класс map--faded) и
-// форма неактивна (добавлен класс notice__form--disabled и все поля формы недоступны, disabled)
 
 var getStartState = function () {
   map.classList.add('map--faded');
@@ -172,27 +161,7 @@ var getStartState = function () {
   }
 };
 
-
-// После того, как на блоке map__pin--main произойдет событие mouseup, форма и карта должны активироваться:
-// У карты убрать класс map--faded
-// Показать на карте метки похожих объявлений, созданные в задании к прошлому разделу
-// У формы убрать класс notice__form--disabled и сделать все поля формы активными
-// var mapPinsTemp = [];
-
-// var openMainPin = function () {
-//   map.classList.remove('map--faded');
-//   mapPins.appendChild(fragmentPins);
-//   noticeForm.classList.remove('notice__form--disabled');
-
-//   for (var i = 0; i < fieldsets.length; i++) {
-//     fieldsets[i].disabled = false;
-//   }
-//   for (var j = 0; j < mapFiltersArray.length; j++) {
-//     mapFiltersArray[j].disabled = false;
-//   }
-// };
-
-mainPin.addEventListener('mouseup', function () {
+var getActiveState = function () {
   map.classList.remove('map--faded');
   mapPins.appendChild(fragmentPins);
   noticeForm.classList.remove('notice__form--disabled');
@@ -202,19 +171,7 @@ mainPin.addEventListener('mouseup', function () {
   for (var j = 0; j < mapFiltersArray.length; j++) {
     mapFiltersArray[j].disabled = false;
   }
-});
-
-var openPopup = function () {
-  mapCard.classList.remove('hidden');
-  document.addEventListener('keydown', popupEscPressHandler);
 };
-
-// Хотел сделать открытие по спейсу, но почему-то не работает
-// mainPin.addEventListener('keydown', function(evt) {
-//   if (evt.keycode === SPACE_KEY) {
-//     openMainPin();
-//   }
-// });
 
 var popupEscPressHandler = function (evt) {
   if (evt.keycode === ESC_KEY) {
@@ -222,19 +179,24 @@ var popupEscPressHandler = function (evt) {
   }
 };
 
-// Если пин объявления в фокусе .map__pin, то диалог с подробностями должен показываться по нажатию кнопки ENTER
-// Когда диалог открыт, то клавиша ESC должна закрывать диалог и деактивировать элемент .map__pin, который был помечен как активный
+var openPopup = function () {
+  mapCard.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscPressHandler);
+};
 
 var closePopup = function () {
   mapCard.classList.add('hidden');
+  if (activePin !== false) {
+    activePin.classList.remove('map__pin--active');
+    activePin = false;
+  }
   document.removeEventListener('keydown', popupEscPressHandler);
 };
 
-
-//получаю стартовое состояние
+//блокирую поля и затемняю экран
 getStartState();
 
-//генерирую рандомные карточки
+//генерирую карточки
 cards = generateCards();
 
 //оборачиваю в DOM-элемент пины
@@ -243,7 +205,7 @@ cards.forEach(function (item) {
 });
 
 //оборачиваю в другой DOM-элемент карточки
-fragmentCards.appendChild(renderCards(cards[getRandomNumber(0, 8)]));
+fragmentCards.appendChild(fillCards(cards[getRandomNumber(0, 8)]));
 
 // вставляю полученный DOM-элемент с карточками в блок .map
 map.appendChild(fragmentCards);
@@ -251,40 +213,40 @@ map.appendChild(fragmentCards);
 // Первым шагом отключите показ по умолчанию первой карточки из набора объявлений
 mapCard.classList.add('hidden');
 
-var activePin = false;
-
-// в момент нажатия на главный пин массив создается, но, если за пределами функции создавать,
-// то он в итоге он успевает создаться до нажатия на главный
-
-var mapPinsTemp = map.querySelectorAll('.map__pin:not(:nth-child(2))');
-console.log(mapPinsTemp);
+//ловлю нажатие на главный пин: разблокирую поля, убираю затемнее, отрисовываю пины
+mainPin.addEventListener('mouseup', function () {
+  getActiveState();
+});
 
 mapPins.addEventListener('click', function (evt) {
   var target = evt.target;
+  var targetId = 0;
   while (target !== mapPins) {
     if (target.tagName === 'BUTTON') {
       if (activePin !== false) {
         activePin.classList.remove('map__pin--active');
       }
-      activePin = target.classList.add('map__pin--active');
+      target.classList.add('map__pin--active');
+      activePin = target;
+      targetId = activePin.id;
       if (!target.classList.contains('map__pin--main')) {
-        renderCards(cards[getRandomNumber(0, 8)]);
+        fillCards(cards[targetId]);
         openPopup();
       }
+      return;
     }
-    return;
+    target = target.parentNode;
   }
-  target = target.parentNode;
 });
 
-// // При нажатии на элемент .popup__close карточка объявления должна скрываться
-// mapCardClose.addEventListener('click', function () {
-//   closePopup();
-// });
+// При нажатии на элемент .popup__close карточка объявления должна скрываться
+mapCardClose.addEventListener('click', function () {
+  closePopup();
+});
 
-// // Если диалог открыт и фокус находится на крестике, то нажатие клавиши ENTER приводит к закрытию диалога
-// mapCardClose.addEventListener('keydown', function (evt) {
-//   if (evt.keycode === ENTER_KEY) {
-//     closePopup();
-//   }
-// });
+// Если диалог открыт и фокус находится на крестике, то нажатие клавиши ENTER приводит к закрытию диалога
+mapCardClose.addEventListener('keydown', function (evt) {
+  if (evt.keycode === ENTER_KEY) {
+    closePopup();
+  }
+});
