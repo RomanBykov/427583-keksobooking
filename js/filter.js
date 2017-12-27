37ec2065ad2a51029acc9aff22cd34892b76cc12
RomanBykov <30844184+RomanBykov@users.noreply.github.com>
2017-12-27 17:42:05 +0300

    Набросок

--- js/filter.js
+++ js/filter.js
@@ -3,9 +3,22 @@
 (function () {
   var mapFilter = document.querySelector('.map__filters-container');
   var filterType = mapFilter.querySelector('#housing-type');
-  var cardsPseudoArr = map.querySelectorAll('.map__card');
-  var arr = [].map.call(cardsPseudoArr, function (it) {
-    return it.h4;
+  var arr = [];
+  var pinsArr = [];
+
+  filterType.addEventListener('change', function () {
+    pinsArr = document.querySelectorAll('.map__pin');
+    arr = [].map.call(pinsArr, function (it) {
+      return it;
+    });
+    arr.shift();
+    console.log(arr);
+    window.card.allCards.forEach(function (item, i) {
+      if (item.offer.type !== 'flat') {
+        return arr[i].classList.add('hidden');
+      }
+      return arr[i].classList.remove('hidden');
+    });
   });
-  console.log(arr);
+
 })();
